1221baea0f2b68092a1b6ea732872aac59ca1d57
InnFen <123163367+InnFen@users.noreply.github.com>
2024-08-18 07:48:27 +0800

    更新 currency.js

--- Script/currency.js
+++ Script/currency.js
@@ -2,6 +2,6 @@
 脚本作者：InnFen
 脚本日期：2024-08-18
 */
-var headers = $request['headers'];
-headers['Cookie'] = $persistentStore.read("Cookie");
-$done({ 'headers': headers });
+var modifiedHeaders = $request['headers'];
+modifiedHeaders['Cookie'] = $persistentStore.read("Cookie");
+$done({'headers': modifiedHeaders});
