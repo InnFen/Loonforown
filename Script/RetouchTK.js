ddc5f5833f879066fd930400d57340436ebcc128
InnFen <123163367+InnFen@users.noreply.github.com>
2024-08-24 11:41:46 +0800

    Create RetouchTK.js

---
+++ Script/RetouchTK.js
@@ -0,0 +1,9 @@
+/*
+脚本作者：InnFen
+脚本日期：2024-08-24
+*/
+
+var headers = $request['headers'];
+delete headers["Cookie"];
+headers['Cookie'] = $persistentStore.read("Retouch_TK");
+$done({ 'headers': headers });
