98257180d78a1a41b5a00f0ed50dd3986683fa26
InnFen <123163367+InnFen@users.noreply.github.com>
2024-08-24 11:28:24 +0800

    Update and rename Pcs_Cookie.js to Pan_Cookie.js

--- Script/Pan_Cookie.js
+++ Script/Pan_Cookie.js
@@ -1,9 +1,9 @@
 /*
 脚本作者：InnFen
-脚本日期：2024-08-23
+脚本日期：2024-08-24
 */
 
 var headers = $request['headers'];
 delete headers["Cookie"];
-headers['Cookie'] = $persistentStore.read("PCS_Cookie");
+headers['Cookie'] = $persistentStore.read("Pan_Cookie");
 $done({ 'headers': headers });
